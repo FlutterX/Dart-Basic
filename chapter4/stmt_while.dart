dbe1471c2eb6685831121a0169210b4b45efaa05
LinXunFeng <598600855@qq.com>
2018-12-12 15:06:32 +0800

    stmt_while

---
+++ chapter4/stmt_while.dart
@@ -0,0 +1,20 @@
+/**
+ * while语句
+ *
+ * while循环
+ * do...while循环
+ */
+
+void main() {
+  int count = 0;
+
+  while(count < 5) {
+    print(count++); // 0 1 2 3 4
+  }
+
+  print('-----$count-----');
+
+  do {
+    print(count--); // 5 4 3 2 1
+  } while(count > 0 && count < 5);
+}
