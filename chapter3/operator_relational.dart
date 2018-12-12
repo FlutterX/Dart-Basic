3b847919a0e5adfd1820717acdb26951115f937f
LinXunFeng <598600855@qq.com>
2018-12-12 14:42:46 +0800

    operator_relational

---
+++ chapter3/operator_relational.dart
@@ -0,0 +1,24 @@
+/**
+ * 关系运算符
+ *
+ * 运算符: ==、!=、>、<、>=、<=
+ * 判断内容是否相同使用 ==
+ */
+void main() {
+  int a = 5;
+  int b = 3;
+
+  print(a == b); // false
+  print(a != b); // true
+  print(a > b);  // true
+  print(a < b);  // false
+  print(a >= b); // true
+  print(a >= b); // true
+
+  String strA = '123';
+  String strB = '321';
+  print(strA == strB); // false
+}
+
+
+
